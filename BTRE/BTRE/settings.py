92a3193d9dd4dffee183f0349f88ecaf4940ac59
afsana1210 <afsanaansari1210@gmail.com>
2020-09-25 16:08:57 +0530

    added modified file

--- BTRE/BTRE/settings.py
+++ BTRE/BTRE/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '$ubcs80yf_jb7zywe0+tp@f=oo4q+#0wkkq#@d%qvhhuyud3$2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
