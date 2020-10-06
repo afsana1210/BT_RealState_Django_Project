6a1a8f3d91926af893263dd7f461a4b66c866d1f
afsana1210 <afsanaansari1210@gmail.com>
2020-10-06 22:38:21 +0530

    pagination

--- BTRE/listings/views.py
+++ BTRE/listings/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from .models import Listing
+from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 # Create your views here.
 def index(request):
     listings=Listing.objects.all()
+    paginator=Paginator(listing,3)
+    page=request.GET.get('page')
+    paged_listings=paginator.get_page(page)
     context={
-        'listings':listings
+        'listings':paged_listings
     }
     return render(request,'listings/listings.html',context)
 
