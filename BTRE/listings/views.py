bfe16903b864d88b48f152a6729329b146923294
afsana1210 <afsanaansari1210@gmail.com>
2020-10-12 21:40:22 +0530

    Dynamic search form choices

--- BTRE/listings/views.py
+++ BTRE/listings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Listing
+from listings.choices import price_choices,bedroom_choices,state_choices
 from django.core.paginator import EmptyPage,PageNotAnInteger,Paginator
 # Create your views here.
 def index(request):
@@ -19,8 +20,17 @@ def listing(request,listing_id):
 
     context={
         'listing':listing
+
     }
     return render(request,'listings/listing.html',context)
 
 def search(request):
-    return render(request,'listings/search.html')
+    queryset_list=Listing.objects.order_by("-list_date")
+
+    context={
+        'price_choices':price_choices,
+        'bedroom_choices':bedroom_choices,
+        'state_choices':state_choices,
+        'listings':queryset_list
+    }
+    return render(request,'listings/search.html',context)
