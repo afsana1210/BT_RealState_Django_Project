134842ea78a6deae343424e3d931d417fef04d17
afsana1210 <afsanaansari1210@gmail.com>
2020-10-20 18:17:54 +0530

    Register and login template

--- BTRE/BTRE/settings.py
+++ BTRE/BTRE/settings.py
@@ -135,3 +135,8 @@ STATICFILES_DIRS=[
 
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
--- BTRE/accounts/views.py
+++ BTRE/accounts/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render,redirect
 
 # Create your views here.
 def register(request):
-    return render(request,'accounts/register.html')
+    if request.method=='POST':
+        return 
+    else:
+       return render(request,'accounts/register.html')
 
 def login(request):
-    return render(request,'accounts/login.html')
+    if request.method=='POST':
+        return 
+    else:
+      return render(request,'accounts/login.html')
 
 def logout(request):
     return redirect('index')
