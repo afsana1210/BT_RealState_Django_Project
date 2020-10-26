17be520120c555745dc83631a8ff832c9e86cc30
afsana1210 <afsanaansari1210@gmail.com>
2020-10-26 22:51:31 +0530

    Dynamic Page Title

--- BTRE/accounts/views.py
+++ BTRE/accounts/views.py
@@ -5,12 +5,12 @@ from django.contrib.auth.models import User
 # Create your views here.
 def register(request):
     if request.method=='POST':
-        first_name=request.POST['first_name']
-        last_name=request.POST['last_name']
-        username=request.POST['username']
-        email=request.POST['email']
-        password=request.POST['password']
-        password2=request.POST['password2']
+        first_name=request.POST.get('first_name')
+        last_name=request.POST.get('last_name')
+        username=request.POST.get('username')
+        email=request.POST.get('email')
+        password=request.POST.get('password')
+        password2=request.POST.get('password2')
 
         if password == password2:
             if User.objects.filter(username=username).exists():
@@ -36,8 +36,8 @@ def register(request):
 
 def login(request):
     if request.method=='POST':
-        username=request.POST['username']
-        password=request.POST['password']
+        username=request.POST.get('username')
+        password=request.POST.get('password')
 
         user=auth.authenticate(username=username,password=password)
 
